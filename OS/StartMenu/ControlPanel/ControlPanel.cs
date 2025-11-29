82475a182e0d9a72fd1006f056cd584077dfa6bf
Александр <sasharuchkin2006@vk.com>
2025-11-29 13:46:58 +0300

    Add files via upload

---
+++ OS/StartMenu/ControlPanel/ControlPanel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OS
+{
+    public partial class ControlPanel : Form
+    {
+        public ControlPanel()
+        {
+            InitializeComponent();
+        }
+        private void ControlPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            UserAdd UserMenu = new UserAdd();
+            UserMenu.Show();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            UserChange userChange = new UserChange();
+            userChange.Show();
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            UserDelete userDelete = new UserDelete();
+            userDelete.Show();
+        }
+    }
+}
